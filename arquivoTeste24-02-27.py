c8ff763ee15fca7d464230c959148fda35196a33
Antonio Oliveira <antoniovini47@gmail.com>
2024-02-27 22:15:39 -0300

    tentativa do primeiro Pull Request

--- arquivoTeste24-02-27.py
+++ arquivoTeste24-02-27.py
@@ -7,4 +7,4 @@ variavelDeVerdade = 10
 print(variavelDeVerdade)
 variavelDeVerdade='string'
 print(variavelDeVerdade)
-print("arquivoAdicional adicionado");
+print("arquivoAdicional.py adicionado");

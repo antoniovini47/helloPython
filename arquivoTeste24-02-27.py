a55b1194ff5eec0851e3d3caae311547f4218724
Antonio Oliveira <antoniovini47@gmail.com>
2024-02-27 22:03:43 -0300

    primeiro envio teste de att

---
+++ arquivoTeste24-02-27.py
@@ -0,0 +1,9 @@
+print("teste texto", 1 + 1)
+item = "bike"
+print(item)
+numeroQualquer = 12
+print(numeroQualquer)
+variavelDeVerdade = 10
+print(variavelDeVerdade)
+variavelDeVerdade='string'
+print(variavelDeVerdade)
